50a0d6782edd49a761bb867485ea0105694ea270
zwk <conquer.wkzhang@sjtu.edu.cn>
2025-07-17 23:08:25 +0800

    tiny change

---
+++ run_shell_derive_frames.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('python derive_gs_frames.py --dataset_dir ../data_video/immersive --scene_list 01_Welder 02_Flames 04_Truck 09_Alexa 10_Alexa 11_Alaxa 12_Cave --start_frame 1 --end_frame 99')
